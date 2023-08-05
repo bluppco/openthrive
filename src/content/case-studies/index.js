4933def2f648a10977b2d892008dab4f670711ed
Mohit <mohit@hey.com>
2023-08-05 11:32:58 +0530

    x

---
+++ src/content/case-studies/index.js
@@ -0,0 +1,6 @@
+export const CASE_STUDIES = [{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Vecmocon-Featured-Image.png?quality=50",
+    company: "Vecmocon Technologies",
+    description: "Vecmocon had some really smart products around EV battery technology, but their website was unable to convey the right message.",
+    link: ""
+}]
