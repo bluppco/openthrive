b6076601476c93aa5b91c026d27f3ff628158a6f
Mohit <mohit@hey.com>
2023-08-05 11:48:58 +0530

    x

--- src/content/case-studies/index.js
+++ src/content/case-studies/index.js
@@ -3,4 +3,44 @@ export const CASE_STUDIES = [{
     company: "Vecmocon Technologies",
     description: "Vecmocon had some really smart products around EV battery technology, but their website was unable to convey the right message.",
     link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Prad-Automotive-Featured-Image.png?quality=50",
+    company: "Prad Automotive",
+    description: "Transformation of an outdated website into two separate functions - Complete B2B marketing project with B2C retail integration.",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Repair-Pricer-Featured-Image.png?quality=50",
+    company: "Repair Pricer",
+    description: "This Real Estate SaaS company had the coolest product in the industry but saw a slow growth rate.",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-RaySecur-Featured-Image.png?quality=50",
+    company: "RaySecur",
+    description: "South-facing business graphs turn North when B2B Marketing (ABM-focused) setup is in place, automation is the backbone, and the sales-enabled.",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Printrove-Featured-Image.png?quality=50",
+    company: "Printrove",
+    description: "Streamlined their B2B marketing and website - 100% organic results, faster conversions, and a growing graph of leads at 0$",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Optimiz-Featured-Image.png?quality=50",
+    company: "Optimiz",
+    description: "Built the B2B inbound marketing growth foundation that defined every action the team took from the CEO to the website",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Finaxar-Featured-Image-1-min.png?quality=50",
+    company: "Finaxar",
+    description: "B2B Marketing of a financing platform for small businesses in Asia, leading to high engagement and retention",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-SMB-Lending-Featured-Image.png?quality=50",
+    company: "Small Business Financing Company",
+    description: "Designed & implemented the complex marketing and sales automation to qualify prospects faster, and make the sales cycles shorter",
+    link: ""
+},{
+    cover: "https://blupp.b-cdn.net/openthrive/Case-Study-Finaxar-Featured-Image-1-min.png?quality=50",
+    company: "FeverScreen",
+    description: "Go-to-Market within a month, immediate response to COVID-19 through B2B website, marketing, and paid ads ",
+    link: ""
 }]
